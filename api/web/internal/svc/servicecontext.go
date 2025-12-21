283c8f80d385e8360486f2640cd4e6c95068de9d
Allen <tutengdihuang@163.com>
2025-12-21 15:22:16 +0800

    update

--- api/web/internal/svc/servicecontext.go
+++ api/web/internal/svc/servicecontext.go
@@ -2,26 +2,25 @@ package svc
 
 import (
 	"service_test/api/web/internal/config"
-	"service_test/rpc/user/userclient"
 	"service_test/rpc/product/productclient"
 	"service_test/rpc/trade/tradeclient"
+	"service_test/rpc/user/userclient"
 
 	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
-	Config    config.Config
-	UserRpc   userclient.User
+	Config     config.Config
+	UserRpc    userclient.User
 	ProductRpc productclient.Product
-	TradeRpc  tradeclient.Trade
+	TradeRpc   tradeclient.Trade
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config:    c,
-		UserRpc:   userclient.NewUser(zrpc.MustNewClient(c.UserRpc).Conn()),
-		ProductRpc: productclient.NewProduct(zrpc.MustNewClient(c.ProductRpc).Conn()),
-		TradeRpc:  tradeclient.NewTrade(zrpc.MustNewClient(c.TradeRpc).Conn()),
+		Config:     c,
+		UserRpc:    userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
+		ProductRpc: productclient.NewProduct(zrpc.MustNewClient(c.ProductRpc)),
+		TradeRpc:   tradeclient.NewTrade(zrpc.MustNewClient(c.TradeRpc)),
 	}
 }
-
